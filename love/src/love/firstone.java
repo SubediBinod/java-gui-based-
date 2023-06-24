4737a136d6348f0dc664499ed911c3ac3031b452
Binod <bidhaksubedi77@gmail.com>
2023-06-24 13:55:15 +0530

    I have done it

---
+++ love/src/love/firstone.java
@@ -0,0 +1,17 @@
+package love;
+import javax.swing.JOptionPane;
+
+public class firstone {
+
+	public static void main(String[] args) {
+		do {
+			String s= JOptionPane.showInputDialog("wanna be my valentine 😀"+"  yes/  no????");
+			if(s.equals("yes")||s.equals("YES")) {
+				JOptionPane.showMessageDialog(null, "😉😉😉😉😉😉 thankyou love ;)");
+				break;
+			}
+		}while(true);
+
+	}
+
+}
